b03bd1289fe6acb35dbcf97ca485532e221fb005
praneeth-123 <praneeth.jayawardena@ymail.com>
2015-06-08 08:37:11 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,233 @@
+package com.abc_company;
+
+import java.util.Date;
+
+public class Employee {
+	private int empNo;
+	private String firstName;
+	private String lastName;
+	private Date joinDate;
+	private String nic;
+	private int age;
+	private char sex;
+	private String designation;
+	private String add1;
+	private String add2;
+	private String add3;
+	private int mobile;
+	private int office;
+	private String email;
+	private int viber;
+	private String skype;
+	private String facebook;
+	private String twitter;
+	private String google_plus;
+	private String depend_name;
+	private String relationship;
+	private String dependancy;
+	private String dob;
+	private String gender;
+	private int deptNo;
+
+	// Generate Getters and Setters
+	public int getEmpNo() {
+		return empNo;
+	}
+
+	public void setEmpNo(int empNo) {
+		this.empNo = empNo;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getJoinDate() {
+		return joinDate;
+	}
+
+	public void setJoinDate(Date joinDate) {
+		this.joinDate = joinDate;
+	}
+
+	public String getNic() {
+		return nic;
+	}
+
+	public void setNic(String nic) {
+		this.nic = nic;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public char getSex() {
+		return sex;
+	}
+
+	public void setSex(char sex) {
+		this.sex = sex;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public String getAdd1() {
+		return add1;
+	}
+
+	public void setAdd1(String add1) {
+		this.add1 = add1;
+	}
+
+	public String getAdd2() {
+		return add2;
+	}
+
+	public void setAdd2(String add2) {
+		this.add2 = add2;
+	}
+
+	public String getAdd3() {
+		return add3;
+	}
+
+	public void setAdd3(String add3) {
+		this.add3 = add3;
+	}
+
+	public int getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(int mobile) {
+		this.mobile = mobile;
+	}
+
+	public int getOffice() {
+		return office;
+	}
+
+	public void setOffice(int office) {
+		this.office = office;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getViber() {
+		return viber;
+	}
+
+	public void setViber(int viber) {
+		this.viber = viber;
+	}
+
+	public String getSkype() {
+		return skype;
+	}
+
+	public void setSkype(String skype) {
+		this.skype = skype;
+	}
+
+	public String getFacebook() {
+		return facebook;
+	}
+
+	public void setFacebook(String facebook) {
+		this.facebook = facebook;
+	}
+
+	public String getTwitter() {
+		return twitter;
+	}
+
+	public void setTwitter(String twitter) {
+		this.twitter = twitter;
+	}
+
+	public String getGoogle_plus() {
+		return google_plus;
+	}
+
+	public void setGoogle_plus(String google_plus) {
+		this.google_plus = google_plus;
+	}
+
+	public String getDepend_name() {
+		return depend_name;
+	}
+
+	public void setDepend_name(String depend_name) {
+		this.depend_name = depend_name;
+	}
+
+	public String getRelationship() {
+		return relationship;
+	}
+
+	public void setRelationship(String relationship) {
+		this.relationship = relationship;
+	}
+
+	public String getDependancy() {
+		return dependancy;
+	}
+
+	public void setDependancy(String dependancy) {
+		this.dependancy = dependancy;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getDeptNo() {
+		return deptNo;
+	}
+
+	public void setDeptNo(int deptNo) {
+		this.deptNo = deptNo;
+	}
+
+}
